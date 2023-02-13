224f30920d771a9fe250b8ff8841fea65ed8e0d9
Elodie RABIBISOA <elodie.rabibisoa@gmail.com>
2023-02-13 22:38:17 +0100

    init project category manager

---
+++ src/main/java/com/test/categorymanager/CategorymanagerApplication.java
@@ -0,0 +1,13 @@
+package com.test.categorymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategorymanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategorymanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/categorymanager/CategorymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.categorymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategorymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
