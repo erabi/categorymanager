96c58c01da5c7bd6f23e4e82f471071685eaa477
Elodie RABIBISOA <elodie.rabibisoa@gmail.com>
2023-02-14 00:33:25 +0100

    categorymanager: database setup

--- src/main/java/com/test/categorymanager/CategorymanagerApplication.java
+++ src/main/java/com/test/categorymanager/CategorymanagerApplication.java
@@ -2,12 +2,15 @@ package com.test.categorymanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ LiquibaseProperties.class })
 public class CategorymanagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CategorymanagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CategorymanagerApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/test/categorymanager/config/DatabaseConfiguration.java
@@ -0,0 +1,39 @@
+package com.test.categorymanager.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories("com.test.categorymanager.repository")
+@EnableTransactionManagement
+public class DatabaseConfiguration {
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource, LiquibaseProperties liquibaseProperties) {
+
+        SpringLiquibase liquibase = new SpringLiquibase();
+
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:db/changelog/master.xml");
+        liquibase.setContexts(liquibaseProperties.getContexts());
+        liquibase.setDefaultSchema(liquibaseProperties.getDefaultSchema());
+        liquibase.setLiquibaseSchema(liquibaseProperties.getLiquibaseSchema());
+        liquibase.setLiquibaseTablespace(liquibaseProperties.getLiquibaseTablespace());
+        liquibase.setDatabaseChangeLogLockTable(liquibaseProperties.getDatabaseChangeLogLockTable());
+        liquibase.setDatabaseChangeLogTable(liquibaseProperties.getDatabaseChangeLogTable());
+        liquibase.setDropFirst(liquibaseProperties.isDropFirst());
+        liquibase.setLabelFilter(liquibaseProperties.getLabelFilter());
+        liquibase.setChangeLogParameters(liquibaseProperties.getParameters());
+        liquibase.setRollbackFile(liquibaseProperties.getRollbackFile());
+        liquibase.setTestRollbackOnUpdate(liquibaseProperties.isTestRollbackOnUpdate());
+
+        return liquibase;
+    }
+}
+
